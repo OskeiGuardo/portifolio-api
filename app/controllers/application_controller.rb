95d8df2614bdfb00cea344167d922cf7fd3d9d1f
davi-canuto <davicanutogregorio@gmail.com>
2023-05-25 18:11:02 -0300

    fix: fixes set current user in login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::API
-  before_action :set_current_user
-
   private
 
   def set_current_user
---
+++ app/controllers/base_controller.rb
@@ -0,0 +1,3 @@
+class BaseController < ApplicationController
+  before_action :set_current_user
+end
